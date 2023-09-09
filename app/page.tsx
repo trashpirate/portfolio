dc0c716c84a807e0cb2affd8dc0a58a1a4136113
trashpirate <nadinaoates@gmail.com>
2023-09-09 14:46:21 -0500

    add copyright

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import Intro from "@/components/intro";
 import styles from "./page.module.css";
-import { Content } from "next/font/google";
 import About from "@/components/about";
 import Socials from "@/components/socials";
 import Experience from "@/components/experience";
 import Projects from "@/components/projects";
 import Navigation from "@/components/navigation";
-import Footer from "@/components/footer";
 
 export default function Home() {
   return (
