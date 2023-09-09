13191290ccc06d7f9987f0cdf374ee88f3796d87
trashpirate <nadinaoates@gmail.com>
2023-09-09 14:24:36 -0500

    adding pdf resume

--- components/experience/index.tsx
+++ components/experience/index.tsx
@@ -2,7 +2,6 @@ import styles from "./experience.module.css";
 import jobData from "./experience.json";
 import Card from "./card";
 import { BsArrowRight } from "react-icons/bs";
-import { AiOutlineLink } from "react-icons/ai";
 
 export default function Experience() {
   return (
@@ -11,7 +10,26 @@ export default function Experience() {
         <Card key={job.id} {...job}></Card>
       ))}
       <div className={styles.container_resume}>
-        <a href="https://www.linkedin.com/in/nadinaoates/details/experience/">View Full Resume </a>
+        <a
+          href={"/resume_nadinaoates_web3_datascientist.pdf"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Download Resume{" "}
+        </a>
+        <p>
+          <BsArrowRight
+            style={{
+              verticalAlign: "middle",
+              margin: "auto 10px",
+            }}
+          />
+        </p>
+      </div>
+      <div className={styles.container_resume}>
+        <a href={"https://www.linkedin.com/in/nadinaoates/details/experience/"}>
+          View on LinkedIn{" "}
+        </a>
         <p>
           <BsArrowRight
             style={{
