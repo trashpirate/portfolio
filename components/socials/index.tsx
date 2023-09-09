c48a763375b17deae01dac2a6971ffd93d6762d8
trashpirate <nadinaoates@gmail.com>
2023-09-09 14:31:06 -0500

    add telegram link

--- components/socials/index.tsx
+++ components/socials/index.tsx
@@ -8,6 +8,7 @@ export default function Socials() {
       <Icon network="twitter" link="https://twitter.com/N0_crypto"></Icon>
       <Icon network="youtube" link="https://youtube.com/@N0_crypto"></Icon>
       <Icon network="github" link="https://github.com/trashpirate"></Icon>
+      <Icon network="telegram" link="https://t.me/oatestalkcrypto"></Icon>
     </div>
   );
 }
