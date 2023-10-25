397919eb6e96a2de977dc1e5fe2735f4f9b326b3
trashpirate <nadinaoates@gmail.com>
2023-10-25 15:05:49 -0500

    fix typo

--- components/about/index.tsx
+++ components/about/index.tsx
@@ -14,8 +14,8 @@ export default function About() {
       <p>
         Currently, I am a Full Stack and Blockchain Developer based in Chicago. I have over 3 years
         of experience in
-        <em>Crypto</em>, <em>NFTs</em>, <em>DeFi</em>, and <em>Web 3.0</em>. I've led projects from
-        concept to production, developed NFT minting dapps, and my latest achievement was to
+        <em>Crypto</em>, <em>NFTs</em>, <em>DeFi</em>, and <em>Web 3.0</em>. I{"'"}ve led projects
+        from concept to production, developed NFT minting dapps, and my latest achievement was to
         increase the gas efficiency of a wildly used ERC20 token contract by 100%. My skills span{" "}
         <em>JavaScript</em>, <em>TypeScript</em>, <em>Node.js</em>, <em>React/Next.js</em>,
         <em>Solidity</em>, <em>Hardhat</em>, <em>Ethers.js</em>, and <em>Slither</em>.
@@ -23,7 +23,7 @@ export default function About() {
       <br />
       <p>
         My versatile skill set extends beyond web and blockchain development to data analysis and
-        computational research, where I've managed projects, released novel simulation software,
+        computational research, where I{"'"}ve managed projects, released novel simulation software,
         published impactful journal articles, and received a research progress award at Northwestern
         University. I also have industry experience working on machine learning and deep learning
         projects using <em>Python</em> - <em>PyTorch, Scikit-Learn</em>, and{" "}
@@ -31,15 +31,15 @@ export default function About() {
       </p>
       <br />
       <p>
-        In addition to my professional work, I'm dedicated to promoting Web3 adoption. I co-founded
-        a multople web3 communities, run a crypto-themed{" "}
+        In addition to my professional work, I{"'"}m dedicated to promoting Web3 adoption. I
+        co-founded a multople web3 communities, run a crypto-themed{" "}
         <a href="https://www.youtube.com/@N0_crypto">YouTube Channel</a> with over 150 subscribers,
         and regularly host{" "}
-        <a href="https://oatestalkcrypto.eventbrite.com">crypto community events</a> in Chicago. I'm
-        excited about the potential of blockchain and decentralized technologies and am dedicated to
-        bringing Web3 adoption to the masses. If you're looking for a versatile developer who can
-        take your projects to the next level, let's connect and discuss how I can contribute to your
-        team.
+        <a href="https://oatestalkcrypto.eventbrite.com">crypto community events</a> in Chicago. I
+        am excited about the potential of blockchain and decentralized technologies and am dedicated
+        to bringing Web3 adoption to the masses. If you{"'"}re looking for a versatile developer who
+        can take your projects to the next level, let{"'"}s connect and discuss how I can contribute
+        to your team.
       </p>
     </div>
   );
