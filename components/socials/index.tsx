210ba080e818feb28b5c6d2d146c084b6a7ba62f
trashpirate <nadinaoates@gmail.com>
2024-09-05 14:36:34 -0500

    update socials

--- components/socials/index.tsx
+++ components/socials/index.tsx
@@ -5,10 +5,9 @@ export default function Socials() {
   return (
     <div className={styles.container}>
       <Icon network="linkedin" link="https://linkedin.com/in/nadinaoates"></Icon>
-      <Icon network="twitter" link="https://twitter.com/N0_crypto"></Icon>
-      <Icon network="youtube" link="https://youtube.com/@N0_crypto"></Icon>
-      <Icon network="github" link="https://github.com/trashpirate"></Icon>
+      <Icon network="twitter" link="https://twitter.com/0xTrashPirate"></Icon>
       <Icon network="telegram" link="https://t.me/EARNServices"></Icon>
+      <Icon network="github" link="https://github.com/trashpirate"></Icon>
     </div>
   );
 }
